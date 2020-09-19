b1a742ccb4a0d00387b5dbeebf0421ee17393c37
programmertaha <programmer.taha.1y@gmail.com>
2020-09-19 05:49:24 +0300

    adding function

---
+++ findMyKeys/script.js
@@ -0,0 +1,4 @@
+const findMyKeys = arr => arr.findIndex(elem => elem === 'keys');
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+
+console.log(findMyKeys(randomStuff));
